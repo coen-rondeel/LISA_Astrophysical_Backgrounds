FEAT: analytical variance estimation + metallicity fixes

src/lisaastrophysicalbackgrounds/star_formation_history.py
@@ -15,21 +15,36 @@ class StarFormationHistory():
             cosmo (Cosmology): The considered cosmology as initialized in the BackgroundCosmolgy class.
         """
         self._config: dict = config
-
-        self.z_high_res = jnp.linspace(0, 12, int(1e4))
+        self.get_metallicities()
         
+        #* Setup high resolution grid for interpolation
+        self.z_high_res = jnp.linspace(0, 10, int(1e4))
         age_func = getattr(cosmo, 'age')
         self.age_high_res = jnp.array(age_func(self.z_high_res).value * 1000)  # in Myr
-        
         self._flip_age = jnp.flip(self.age_high_res)
         self._flip_z = jnp.flip(self.z_high_res)
 
+        #* Get SFH function
         sfh_name = self._config['SFH']['SFH_name']
+        
+        if sfh_name == "chruslinska_and_nelemans":
+            self._setup_chruslinska_data()
+        
         self._psi_function: Callable = getattr(self, sfh_name)
         
-        if sfh_name == "data":
-            self._setup_grid_data()
-            
+
+    
+    def get_metallicities(self) -> None:
+        """Sets up the metallicity grid using the config file is N_Zs > 1.
+        """
+        self.SFH_Zs = jnp.atleast_1d(self._config["SFH"]["SFH_metallicities"])
+        self.SFH_Z_bins = jnp.atleast_1d(self._config["SFH"]["SFH_metallicity_bins"])
+        
+        if len(self.SFH_Zs) > 1:
+            assert len(self.SFH_Z_bins) == len(self.SFH_Zs) + 1, "Metallicity grid not initiallized correctly, please check."
+        else: 
+            self.SFH_Z_bins = None
+        
 
     def delayed_SFH(self, age: jax.Array, delay: jax.Array, metallicity: Optional[jax.Array] = None) -> jax.Array:
         """Calculates the delayed star formation rate for a given age of the universe and the delay of the formed stars.
@@ -74,7 +89,6 @@ class StarFormationHistory():
 
     def analytical_metallicity_distribution(self, z: jax.Array, metallicity: jax.Array) -> jax.Array:
         """A standard log-normal analytical metallicity distribution P(Z | z).
-        (You can update mean_logZ and sigma_logZ to match Langer & Norman 2006 or similar).
         """
         mean_logZ: jax.Array = -0.2 * z 
         sigma_logZ: float = 0.5
@@ -110,17 +124,19 @@ class StarFormationHistory():
         mu = jnp.log(mean_Z) - (sigma**2) / 2.0
         
         # Safely handle Z <= 0 to prevent NaNs in jnp.log
-        Z_safe = jnp.where(metallicity > 0, metallicity, 1e-12)
-        ln_Z = jnp.log(Z_safe)
+        ln_Z = jnp.log(metallicity)
         
         # (Eq. 7 from Neijssel et al. 2019)
-        p_Z = (1.0 / (Z_safe * sigma * jnp.sqrt(2.0 * jnp.pi))) * jnp.exp(
+        p_Z = (1.0 / (metallicity * sigma * jnp.sqrt(2.0 * jnp.pi))) * jnp.exp(
             -0.5 * jnp.square((ln_Z - mu) / sigma)
         )
         # TODO add deltaZ factor
         print("WARNING: deltaZ factor not yet implemented")
         return sfr_z * p_Z
     
+    
 
-    def _setup_grid_data(self) -> None:
+    def _setup_chruslinska_data(self) -> None:
         path_to_sfh_data = self._config['SFH']['SFH_path']
+        
+        

src/lisaastrophysicalbackgrounds/utils.py
@@ -1,6 +1,7 @@
+import jax
 import yaml
+import jax.numpy as jnp
 import matplotlib.pyplot as plt
-from numpy.typing import ArrayLike
 from pathlib import Path
 
 def get_config(path):
@@ -16,10 +17,14 @@ def get_config(path):
         return yaml.load(stream, yaml.FullLoader)
     
 
-def get_GWB_plot(frequencies: ArrayLike, omega_f: ArrayLike, save_path: Path):
-    plt.loglog(frequencies, omega_f)
-    plt.savefig(save_path, dpi=150, bbox_inches="tight")
-    plt.ylabel(r"GWB strength $\Omega_\rm{GW}(f)$")
+def get_GWB_plot(frequencies: jax.Array, omega_f: jax.Array, var_f: jax.Array, save_path: Path):
+    plt.loglog(frequencies, omega_f, label="Expectation value")
+    plt.fill_between(frequencies, omega_f - jnp.sqrt(var_f), omega_f + jnp.sqrt(var_f), alpha=0.35, label="1-sigma interval", color="tab:blue")
+    plt.fill_between(frequencies, omega_f - 2.0*jnp.sqrt(var_f), omega_f + 2.0*jnp.sqrt(var_f), alpha=0.25, label="2-sigma interval", color="tab:blue")
+    plt.fill_between(frequencies, omega_f - 3.0*jnp.sqrt(var_f), omega_f + 3.0*jnp.sqrt(var_f), alpha=0.15, label="3-sigma interval", color="tab:blue")
+    plt.ylabel(r"GWB strength $\Omega_{\rm{GW}}(f)$")
     plt.xlabel(r"Frequency $f$")
+    plt.legend()
+    plt.savefig(save_path, dpi=150, bbox_inches="tight")
     plt.close()
     